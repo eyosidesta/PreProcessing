9a9bf02469a9171ee90ad55f8cea1cc70158d774
eyosidesta <mamesmsdesta@gmail.com>
2024-03-04 22:44:46 -0500

    preprocess method added

--- PreprocessAndNormalize.py
+++ PreprocessAndNormalize.py
@@ -3,82 +3,9 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
-def preprocess_image(image_path, target_size=(512, 512)):
-    # Read the image from file
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)  # assuming RGB images
-
-    # Resize the image to the target size
-    # image = tf.image.resize(image, target_size)
-
-    # Normalize pixel values to be in the range [0, 1]
-    image = tf.image.convert_image_dtype(image, tf.float32)
-
-    return image
-
-def preprocess_and_normalize_images(image_folder, target_size=(512, 512)):
-    normalized_images = []
-    count = 0
-
-    # Iterate over all files in the folder
-    for filename in os.listdir(image_folder):
-        count += 1
-        if filename.endswith(('.jpg', '.jpeg', '.png')):  # Ensure the file is an image
-            # Construct the full image path
-            full_image_path = os.path.join(image_folder, filename)
-            print("names: ", full_image_path)
-
-            # Read and preprocess the image
-            image = preprocess_images(full_image_path, target_size)
-            # print("image is: ", image)
-            # plt.subplot(1, 2, 1)
-
-            # plt.title("Processed Image")
-            # plt.show(image, cmap='gray')
-            # if (count == 1):
-            #     break
-
-            # Append the normalized image to the list
-            normalized_images.append(image)
-            # normalized_images.append(full_image_path)
-
-    return normalized_images
-
 def preprocess_images(full_image_path, target_size=(512, 212)):
      # Read the image using OpenCV
     image = cv2.imread(full_image_path)
 
     # Convert BGR to RGB (OpenCV uses BGR by default)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Resize the image to the target size
-    image = cv2.resize(image, (512, 512))
-
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-
-
-    # Normalize pixel values to be in the range [0, 1]
-    image = image / 255.0
-    return image
-
-def plot_images(images, num_cols=3):
-    num_images = len(images)
-    num_rows = (num_images + num_cols - 1) // num_cols
-
-    plt.figure(figsize=(15, 5 * num_rows))
-    i = 0
-
-    # for i in range(num_images):
-    plt.subplot(num_rows, num_cols, i + 1)
-    plt.imshow(images[i])
-    plt.title(f"Image {i + 1}")
-    plt.axis('off')
-
-    plt.show()
-# Example usage:
-image_folder = "/Users/eyosiasdesta/Desktop/GuardRails/Backend/Normalization/guardImages"
-normalized_images = preprocess_and_normalize_images(image_folder)
-plot_images(normalized_images)
-
-
-# `normalized_images` is now a list containing the normalized versions of all images in the folder
