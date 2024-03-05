9e6ace01644fad828ea0c23ae490f1137a6d9d29
eyosidesta <mamesmsdesta@gmail.com>
2024-03-04 22:45:20 -0500

    resize feature added

--- PreprocessAndNormalize.py
+++ PreprocessAndNormalize.py
@@ -9,3 +9,12 @@ def preprocess_images(full_image_path, target_size=(512, 212)):
 
     # Convert BGR to RGB (OpenCV uses BGR by default)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Resize the image to the target size
+    image = cv2.resize(image, (512, 512))
+
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+
+    # Normalize pixel values to be in the range [0, 1]
+    image = image / 255.0
+    return image
